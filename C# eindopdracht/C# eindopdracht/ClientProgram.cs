4e93558032b7b4da0ef1e5d9d2ca606c63ce10fc
Rick <rrhvanhoof@gmail.com>
2024-10-24 13:46:44 +0200

    init shit
    init bruv

---
+++ C# eindopdracht/C# eindopdracht/ClientProgram.cs
@@ -0,0 +1,52 @@
+﻿
+using System.Net.Sockets;
+using System;
+using System.Text;
+
+namespace Client {
+    class ClientProgram {
+        static void Main(string[] args) {
+
+            TcpClient client = new TcpClient("localhost", 1212);
+            setUsername(client);
+
+            Thread threat = new Thread(ReadMessage);
+            threat.Start(client);
+
+            while (true) {
+
+            }
+        }
+
+        private static void setUsername(TcpClient client) {
+            Console.WriteLine("Type your username");
+            string message = Console.ReadLine();
+
+            WriteMessage(client, message);
+        }
+
+        private static void WriteMessage(TcpClient client, string message) {
+            var stream = new StreamWriter(client.GetStream(), Encoding.ASCII);
+
+            stream.WriteLine(message);
+            stream.Flush();
+        }
+
+        static void ReadMessage(object obj) {
+            TcpClient client = obj as TcpClient;
+
+            var stream = new StreamReader(client.GetStream(), Encoding.ASCII);
+
+            while (true) {
+                string line = stream.ReadLine();
+
+                if (line != null) {
+                    Console.WriteLine(line);
+                }
+
+                Thread.Sleep(100);
+            }
+            
+        }
+    }
+}
---
+++ C# eindopdracht/Server/ServerProgram.cs
@@ -0,0 +1,58 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server {
+    static void Main(string[] args) {
+        IPAddress localhost = IPAddress.Parse("127.0.0.1");
+        TcpListener listener = new TcpListener(localhost, 1212);
+        int connetions = 0;
+
+        listener.Start();
+
+        while (true) {
+            Console.WriteLine("Waiting for Connection");
+
+            TcpClient client = listener.AcceptTcpClient();
+
+            if (client != null) {
+                connetions++;
+                Console.WriteLine(connetions);
+            }
+
+            Thread thread = new Thread(ClientHandler);
+            thread.Start(client);
+        }
+    }
+
+    static void ClientHandler(object obj) {
+        TcpClient client = obj as TcpClient;
+
+        bool done = false;
+        while (!done) {
+            string received = ReadTextMessage(client);
+            Console.WriteLine(received);
+
+            done = received.Equals("Bye");
+            if (done) WriteTextMessage(client, "Haudoe");
+            else WriteTextMessage(client, "OK");
+
+        }
+        client.Close();
+        Console.WriteLine("Connection lost");
+    }
+
+    public static void WriteTextMessage(TcpClient client, string text) {
+        var stream = new StreamWriter(client.GetStream(), Encoding.ASCII);
+        {
+            stream.WriteLine(text);
+            stream.Flush();
+        }
+    }
+    public static string ReadTextMessage(TcpClient client) {
+        var stream = new StreamReader(client.GetStream(), Encoding.ASCII);
+        {
+            return stream.ReadLine();
+        }
+    }
+}
