ef40aea6f81be2c3d3cb5f7edf92a7cf72559d68
Rick <rrhvanhoof@gmail.com>
2024-10-26 17:07:31 +0200

    big stuff, GUI gemaakt

---
+++ C# eindopdracht/ClientWinForm/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace ClientWinForm {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+            
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.clientBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.activeConnections = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.clientBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // clientBindingSource
+            // 
+            this.clientBindingSource.CurrentChanged += new System.EventHandler(this.clientBindingSource_CurrentChanged);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(226, 426);
+            this.textBox.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(431, 20);
+            this.textBox.TabIndex = 1;
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // activeConnections
+            // 
+            this.activeConnections.Items.AddRange(new object[] {
+            "Active connections in this session:"});
+            this.activeConnections.Location = new System.Drawing.Point(0, 0);
+            this.activeConnections.Name = "activeConnections";
+            this.activeConnections.Size = new System.Drawing.Size(210, 446);
+            this.activeConnections.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 444);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.activeConnections);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Chat client";
+            ((System.ComponentModel.ISupportInitialize)(this.clientBindingSource)).EndInit();
+            this.ResumeLayout(true);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.ListBox activeConnections;
+        private System.Windows.Forms.ListBox chatWindow;
+        private System.Windows.Forms.BindingSource clientBindingSource;
+    }
+}
+
---
+++ C# eindopdracht/ClientWinForm/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientWinForm {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        private void textBox_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void clientBindingSource_CurrentChanged(object sender, EventArgs e) {
+
+        }
+    }
+
+    
+}
